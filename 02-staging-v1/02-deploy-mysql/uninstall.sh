0cc3fa149a41ac6ccf5f68ea4dc9ca0525393b2f
HieuNT <hieunt9@gmail.com>
2021-11-10 17:16:01 +0700

    update 10112021

--- 02-staging-v1/02-deploy-mysql/uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# uninstall chart
-kubectl config set-context --current --namespace mysql
-helm uninstall mysql --namespace=mysql
-kubectl delete pvc data-mysql-0
-kubectl delete ns mysql
-
-
